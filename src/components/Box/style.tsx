cb8708cbeda5d532928dc9b0dae3b7ef649fd80d
walterpaulo <walter0paulo@hotmail.com>
2022-05-24 11:01:15 -0300

    styled: image

--- src/components/Box/style.tsx
+++ src/components/Box/style.tsx
@@ -53,6 +53,14 @@ export const Avatar = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
+    cursor: pointer;
+    transition: border-radius 2s;
+
+    :hover{
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        opacity: 0.9;
+        border-radius: 10px;
+    }
     
     img{
         background-image: fixed;
